58e902ebbb6c7a2b7a194bfc6db6cd5052a47b57
ayeshafatima <ayeshafatima@localhost>
2022-02-21 21:41:38 +0530

    BreakAndContinue Statement added in to the project 14-02-2022

---
+++ 14-02-2022 BreakAndContinue statement/src/com/BreakAndContinue/Example2.java
@@ -0,0 +1,16 @@
+package com.BreakAndContinue;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++)
+		{
+			if(i==7)
+			{
+				continue;//continue statement
+			}
+			System.out.println(i);
+		}
+	}
+
+}
